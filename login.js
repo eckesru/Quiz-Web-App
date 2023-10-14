598af9f9338be291d1134ce470a37d5dbb39b0b7
Jan Brauer <jan.brauer@iubh-fernstudium.de>
2023-10-15 00:23:17 +0200

    Frontend Verbesserungen Login

--- login.js
+++ login.js
@@ -1,3 +1,25 @@
+const fehlermeldung = document.getElementById("fehlermeldung");
+const erfolgsmeldung = document.getElementById("erfolgsmeldung");
+
+// Fehlgeschlagene Anmeldung
+fehlermeldung.textContent =
+  "Login fehlgeschlagen. Bitte überprüfe deine Eingaben.";
+
+// Erfolgreiche Anmeldung
+erfolgsmeldung.textContent = "Login erfolgreich!";
+
+// Zeige oder verstecke die Meldungen je nach Bedarf
+function zeigeFehlermeldung() {
+  fehlermeldung.style.display = "block";
+  erfolgsmeldung.style.display = "none";
+}
+
+function zeigeErfolgsmeldung() {
+  fehlermeldung.style.display = "none";
+  erfolgsmeldung.style.display = "block";
+}
+
+// Event-Listener für das Login-Formular hinzufügen
 document
   .getElementById("loginFormular")
   .addEventListener("submit", function (event) {
@@ -7,16 +29,19 @@ document
     const email = document.getElementById("email").value;
     const passwort = document.getElementById("passwort").value;
 
-    // Prüfung des Logins (provisorisch)
+    // Provisorische Prüfung des Logins
     if (email === "example@example.com" && passwort === "password123") {
       // Erfolgreiche Anmeldung
-      console.log("Erfolgreich eingeloggt!");
+      console.log("Login erfolgreich!");
+      zeigeErfolgsmeldung();
 
-      // Weiterleitung zur index.html
-      window.location.href = "frage.html";
+      // Weiterleitung zur frage.html nach einer kurzen Verzögerung
+      setTimeout(function () {
+        window.location.href = "frage.html";
+      }, 2000); // 2000 Millisekunden (2 Sekunden) Verzögerung
     } else {
       // Fehlgeschlagene Anmeldung
-      console.error("Login fehlgeschlagen. Bitte überprüfe deine Eingaben.");
+      zeigeFehlermeldung();
     }
   });
 
