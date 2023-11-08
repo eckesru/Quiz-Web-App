a393755b855d547c766a06c5ac88316543b693fd
Jan Brauer <jan.brauer@iubh-fernstudium.de>
2023-11-08 10:10:49 +0100

    Anpassungen meineInhalte und frage_anzeigen

---
+++ static/lib/js/antwort.js
@@ -0,0 +1,10 @@
+// TinyMCE um aus dem Antworttext ein Rich Text Editor zu machen
+tinymce.init({
+  selector: "#antwortText",
+  plugins: "lists link",
+  toolbar:
+    "undo redo | formatselect | bold italic underline | numlist bullist | link",
+  menubar: false,
+  width: "100%",
+  height: 300,
+});
