6199725217f498e4381a4b62e00817304b69df73
Jan Brauer <jan.brauer@iubh-fernstudium.de>
2023-11-08 11:15:38 +0100

    Löschen-Abfrage angepasst

--- static/lib/js/meineInhalte.js
+++ static/lib/js/meineInhalte.js
@@ -1,21 +1,7 @@
-document
-  .getElementById("loeschen-button")
-  .addEventListener("click", function () {
-    document.getElementById("bestaetigungsBox").style.display = "block";
-  });
-
-document
-  .getElementById("bestaetigen-button")
-  .addEventListener("click", function () {
-    // Hier kannst du den Code für die Lösch-Aktion einfügen
-    document.getElementById("bestaetigungsBox").style.display = "none";
-    alert("Der Eintrag wurde gelöscht."); // Nur ein Beispiel-Alert
-  });
-
-document
-  .getElementById("abbrechen-button")
-  .addEventListener("click", function () {
-    // Hier kannst du optionalen Code einfügen, wenn der Benutzer die Aktion abbricht
-    document.getElementById("bestätigungsBox").style.display = "none";
-    alert("Der Eintrag wurde nicht gelöscht.");
-  });
+function bestaetigeLoeschen() {
+  if (confirm("Sind Sie sicher, dass Sie diese Frage löschen möchten?")) {
+      // Der Benutzer hat "OK" in der Bestätigungsabfrage ausgewählt
+  } else {
+      // Der Benutzer hat "Abbrechen" in der Bestätigungsabfrage ausgewählt, keine Aktion erforderlich.
+  }
+}
