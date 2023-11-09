61c10baff2d263233e73b6b8a8ad3ce04934b064
eckesru <rudi.eckes@gmail.com>
2023-11-09 08:39:42 +0100

    Individuelle Fehlermeldungen beim Login implementiert, Systemsprache auf de-de gesetzt

--- Login/views.py
+++ Login/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+from django.utils.translation import gettext as _
 
 
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST) # Authentifiziert den User
+        form = CustomAuthenticationForm(request, data=request.POST)
+        # Authentifiziert den User
         if form.is_valid():
             user = form.get_user()
             login(request, user)
@@ -19,3 +21,13 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
+
+
+# Eigene AuthenticationForm, um Fehlermeldungen anzupassen
+class CustomAuthenticationForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': _(
+            "Ungültiger Benutzer oder ungültiges Passwort."
+        ),
+        'inactive': _("Benutzer ist inaktiv."),
+    }
