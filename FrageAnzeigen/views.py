0d63270087d58170fd4a8e68fa734723a1183395
eckesru <rudi.eckes@gmail.com>
2023-11-09 10:48:34 +0100

    AntwortErstellen-Funktion implementiert, AntwortBearbeiten und DashboardApp initial angelegt

--- FrageAnzeigen/views.py
+++ FrageAnzeigen/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from Core.models import Frage, Benutzer
+from Core.models import Frage, Benutzer, Antwort
 # from .models import KLASSENNAME, Hier Models importieren!
 
 from django.contrib.auth.decorators import login_required
@@ -11,9 +11,16 @@ from django.contrib.auth.decorators import login_required
 # Leitet User zum Login, wenn nicht eingeloggt
 # Create your views here.
 def frage_anzeigen_view(request, frage_id):
-    frage = Frage.objects.get(id=frage_id)
+    if request.method == "POST":
+        # Falls der antwortErstellen-Button betätigt wurde, dann Antwort-Func.
+        if 'antwortErstellen' in request.POST:
+            antwort_erstellen_(request=request, frage_id=frage_id)
 
-    context = {"frage": frage}
+    frage = Frage.objects.get(id=frage_id)
+    antwort = Antwort.objects.filter(frage=frage).values()
+    context = {"frage": frage,
+               "antwort": antwort}
+    print(antwort)
     return render(request, 'frage_anzeigen.html', context)
 
 
@@ -32,3 +39,19 @@ def frage_anzeigen_view_delete(request, frage_id):
     if reverse_url is None:
         return redirect("/frage/" + str(frage_id) + "/")
     return HttpResponseRedirect(request.META.get("HTTP_REFERER"))
+
+
+def antwort_erstellen_(request, frage_id):
+    user = request.user
+    frage = Frage.objects.get(id=frage_id)
+    antwort_text = request.POST.get('antwortText')
+
+    # Erzeugung des Antwort-Objekts
+    antwort = Antwort.objects.create(
+        user=user,
+        frage=frage,
+        text=antwort_text
+    )
+
+    antwort.save()
+    return redirect("/frage/" + str(frage_id) + "/")
