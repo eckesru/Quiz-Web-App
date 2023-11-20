d48892568ab483d31e4df50ea3e1d0fb003bee0b
Jan Brauer <jan.brauer@iubh-fernstudium.de>
2023-11-20 14:17:49 +0100

    Implementierung Like-Button

--- static/lib/js/button_like.js
+++ static/lib/js/button_like.js
@@ -23,13 +23,11 @@ function frageLiken(frageId) {
 
   //"frage/" + frageId + "/like"
   console.log("Frage ID:", frageId);
-  xhr.open("POST", "/like", true);
-  console.log(xhr.open("POST", "/like", true));
+  xhr.open("POST", "9" + "/like", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.send("csrfmiddlewaretoken={{ csrf_token }}"); // Füge den CSRF-Token hinzu
 }
 
-console.log("Skript geladen");
 /*
 $(document).ready(function () {
   $("#like-button").click(function () {
