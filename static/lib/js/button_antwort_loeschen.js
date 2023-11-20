9635e76c79b9f132f7f15e11aeb350c1fad26961
Jan Brauer <jan.brauer@iubh-fernstudium.de>
2023-11-20 12:33:42 +0100

    Antwort löschen inkl. Bestätigung

--- static/lib/js/button_antwort_loeschen.js
+++ static/lib/js/button_antwort_loeschen.js
@@ -1,5 +1,10 @@
 function antwortLoeschen(frageId, antwortID) {
-  // URL: frage/ID/antwort/ID/delete/
-  window.location.href =
-    "/frage/" + frageId + "/antwort/" + antwortID + "/delete/";
+  var bestaetigung = confirm(
+    "Bist du sicher, dass du diese Antwort löschen möchtest?"
+  );
+  if (bestaetigung) {
+    // URL: frage/ID/antwort/ID/delete/
+    window.location.href =
+      "/frage/" + frageId + "/antwort/" + antwortID + "/delete/";
+  }
 }
