dee598d6f67bb80d14af95013e59b28029fd8999
eckesru <rudi.eckes@gmail.com>
2023-11-30 22:25:25 +0100

    Paginator grundlegend implementiert.

--- Startseite/views.py
+++ Startseite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from Core.models import Frage, Benutzer
 from django.contrib.auth.decorators import login_required
 from django.db.models.functions import TruncDate
+from django.core.paginator import Paginator
 from .utils import get_hot_frage, get_frage_des_tages, get_top_5_users
 
 
@@ -18,13 +19,20 @@ def startseite_view(request):
         annotate(creation_date_only=TruncDate('creation_date')).\
         order_by("-creation_date_only", "-likes")
 
+    # Instanziierung eines Paginators
+    # Damit werden Seiten von Django gemanaged
+    # class Paginator(object_list, per_page, *orphans, *allow_empty_first_page)
+    paginator = Paginator(frage, 10)
+    page = request.GET.get('page')
+    page_frage = paginator.get_page(page)
+
     hot_frage = get_hot_frage()
 
     frage_des_tages = get_frage_des_tages(request.user)
 
     top_5_user = get_top_5_users()
 
-    context = {"frage": frage,
+    context = {"page_frage": page_frage,
                "hot_frage": hot_frage,
                "frage_des_tages": frage_des_tages,
                "top_5_user": top_5_user}
