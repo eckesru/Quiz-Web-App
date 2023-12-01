217bcd3143d7a5420ac1fb2e812e2890eb830d78
eckesru <rudi.eckes@gmail.com>
2023-12-01 23:53:44 +0100

    Punkteberechnung zur  Frage des Tages implementiert.

--- Core/utils.py
+++ Core/utils.py
@@ -1,5 +1,5 @@
 # Datei zur Auslagerung von Methoden
-from .models import Frage, Antwort, Benutzer
+from .models import Frage, Antwort, Benutzer, BenutzerQuesModel
 
 
 def update_points_for_user(user):
@@ -22,18 +22,35 @@ def update_points_for_user(user):
 
     likes_amount = frage_likes + antwort_likes
 
+    # Ermittlung der korrekten Antworten bei teilgenommenen Frage des Tages
+    teilnahmen_frage_des_tages = BenutzerQuesModel.objects.filter(user=user)
+    correct_answers = 0
+    for teilnahme in teilnahmen_frage_des_tages:
+        if (teilnahme.answer == teilnahme.quizfrage.ans):
+            correct_answers += 1
+
+    # Ermittlung der korrekten Antworten bei Quiz der Woche
+    # TODO: Punktermittlung zur Quiz der Woche implementieren
+    correct_quiz_answers = 0
+
     # Gewichte der Punkteberechnung
     frage_mutiplier = 2
     antwort_multiplier = 1
     like_multiplier = 3
+    frage_des_tages_multiplier = 2
+    quiz_der_woche_multiplier = 0.5
 
     # Berechnung der einzelnen Punkte anhand der Gewichte
     frage_points = fragen_amount * frage_mutiplier
     antwort_points = antworten_amount * antwort_multiplier
     like_points = likes_amount * like_multiplier
+    frage_des_tages_points = correct_answers * frage_des_tages_multiplier
+    quiz_der_woche_points = round(correct_quiz_answers *
+                                  quiz_der_woche_multiplier)
 
     # Gesamtsumme der Punkte als Ergebnis zurückgeben
-    points = frage_points + antwort_points + like_points
+    points = frage_points + antwort_points + like_points +\
+        frage_des_tages_points + quiz_der_woche_points
 
     # Rang des Nutzers ermitteln
     rank = get_rank(points)
@@ -43,7 +60,7 @@ def update_points_for_user(user):
 
 
 def get_rank(points):
- 
+
     rangstufen = [
         (10000, "Legende der Lehrbücher"),
         (5000,  "Professor der Problemlösung"),
