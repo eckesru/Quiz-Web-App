2cb186ad5c28260e50fb2214b9302d5a37cda807
Jan Brauer <jan.brauer@iubh-fernstudium.de>
2023-12-03 08:49:39 +0100

    Paginator JavaScript

--- static/lib/js/paginator.js
+++ static/lib/js/paginator.js
@@ -1,18 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   function loadContent(url, targetElementId) {
     fetch(url)
-      .then((response) => response.text())
-      .then((data) => {
-        // Überprüfe, ob das Element existiert, bevor du den Inhalt aktualisierst
+      .then(response => response.text())
+      .then(data => {
         var targetElement = document.getElementById(targetElementId);
         if (targetElement) {
           targetElement.innerHTML = data;
         } else {
-          console.error("Error: Target element not found");
+          console.error('Error: Target element not found');
         }
       })
-      .catch((error) => console.error("Error:", error));
+      .catch(error => console.error('Error:', error));
   }
+
+  // Weitere Funktionen oder Code können hier stehen
 });
 
 console.log("paginator.js geladen");
