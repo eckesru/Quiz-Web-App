bcfa4393362540c7724bd0f13350314631a6b6b4
Jan Brauer <jan.brauer@iubh-fernstudium.de>
2023-12-03 20:03:49 +0100

    Frage des Tages: Statistiken anzeigen

--- ISEF01_Quizsystem/settings.py
+++ ISEF01_Quizsystem/settings.py
@@ -79,7 +79,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'bootstrap4': 'django-bootstrap4',
+                }
         },
+
     },
 ]
 
