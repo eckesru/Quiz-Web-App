464773f250483ec068db233edb4920fc439379f7
eckesru <rudi.eckes@gmail.com>
2023-12-07 21:39:46 +0100

    Paginator auf Meine-Inhalte vollständig implementiert.

--- MeineInhalte/views.py
+++ MeineInhalte/views.py
@@ -14,20 +14,24 @@ def meine_inhalte_view(request):
 
     # Instanziierung des Paginators
     # class Paginator(object_list, per_page, *orphans, *allow_empty_first_page)
-    # TODO: Ermittlung der Seitenzahlen (zwei Objekte) klären
     paginator_frage = Paginator(user_fragen, 10)
+
     page_frage = request.GET.get('seite')
-    page_frage_obj = paginator_frage.get_page(page_frage)
+    # Aufbau: .../?seite=x-y, split um x zu bekommen
+    page_frage_list = page_frage.split("-")
+    page_frage_obj = paginator_frage.get_page(page_frage_list[0])
 
     user_antworten = Antwort.objects.filter(user_id=user).\
         order_by("-creation_date")
 
     # Instanziierung des Paginators
     # class Paginator(object_list, per_page, *orphans, *allow_empty_first_page)
-    # TODO: Ermittlung der Seitenzahlen (zwei Objekte) klären
     paginator_antwort = Paginator(user_antworten, 15)
+
     page_antwort = request.GET.get('seite')
-    page_antwort_obj = paginator_antwort.get_page(page_antwort)
+    # Aufbau: .../?seite=x-y, split um y zu bekommen
+    page_antwort_list = page_antwort.split("-")
+    page_antwort_obj = paginator_antwort.get_page(page_antwort_list[1])
 
     context = {"page_frage": page_frage_obj, "page_antwort": page_antwort_obj}
     return render(request, 'meineInhalte.html', context)
